b06d5c3b647c79d804232d02680066bb24f892c3
Mike Dinder <dindermike@hotmail.com>
2026-03-09 18:25:40 -0700

    More work on static files conversion

--- urls.py
+++ urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -26,4 +28,4 @@ urlpatterns = [
     path('api/v1/restaurants/search/', RestaurantSearchView.as_view(), name='restaurant-search'),
 
     path('', LandingView.as_view(), name='home'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
