a0e6fb2d5cdc9e0c433125aec17d6144be94567e
Mike Dinder <dindermike@hotmail.com>
2026-03-19 17:25:36 -0700

    Adjustments for compress packages in production

--- settings.py
+++ settings.py
@@ -184,6 +184,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+COMPRESS_STORAGE = STATICFILES_STORAGE
+
 # For File Compressing and Minifying
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
